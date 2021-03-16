4fc1848c0e35e71e8ee5525270d8b3a73282dff8
Keitaro Murakami <keitaro.mrkm@gmail.com>
2021-03-16 22:43:41 +0900

    change servo degree from input()

--- servo.py
+++ servo.py
@@ -5,19 +5,18 @@ SERVO_PIN = 23
 
 args = sys.argv
 
-f = 0
-if ( len( args ) > 1 ):
-	if ( args[1].isdecimal() ):
-		p_width = int( args[1] )
-		if ( p_width >= 500 and p_width <= 2500 ):
-			f = 1
-
-if ( f == 1 ):
-	print ("Servo set", p_width )
-	pi = pigpio.pi()
-	pi.set_servo_pulsewidth(SERVO_PIN, p_width)
-
-else:
-	print ("USAGE: python3 servo.py PULSE_WIDTH")
-	print ("  PULSE_WHDHT : Pulse range is 500-2500.")
+pi = pigpio.pi()
+try:
+	while(True):
+		degree = int(input("Please input degree (0-270).\n"))
+		if (degree > 270 or degree < 0):
+			print("Degree range is 0-270")
+			continue
+		p_width = int(degree * 2000/270 + 500)
+		print("Servo set", degree)
+		pi.set_servo_pulsewidth(SERVO_PIN, p_width)
+except KeyboardInterrupt:
+	print("finish")
+	pi.stop()
+	pass
 
